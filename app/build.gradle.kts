398619a5fc03be267769c01073494a20d1e3fe88
Marko Durdev <marko.durdev@erasys.de>
2024-09-03 21:55:42 +0200

    Increase version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
     applicationId = "com.thequicknotes"
     minSdk = 26
     targetSdk = 35
-    versionCode = 3
+    versionCode = 4
     versionName = "1.0.1"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
