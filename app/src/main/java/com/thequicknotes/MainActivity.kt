ce6ad434f4a27d6e0d5d5ffd4b7ac9399eaaf76f
Marko Durdev <marko.durdev@erasys.de>
2024-09-04 10:55:53 +0200

    Implement dark mode switching

--- app/src/main/java/com/thequicknotes/MainActivity.kt
+++ app/src/main/java/com/thequicknotes/MainActivity.kt
@@ -3,18 +3,26 @@ package com.thequicknotes
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.collectAsState
+import com.thequicknotes.data.repositories.pref.NotesDataStoreRepository
 import com.thequicknotes.navigation.QuickNotesNavHost
 import com.thequicknotes.uicomponents.theme.theme.AppTheme
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+  @Inject
+  lateinit var dataStoreRepo: NotesDataStoreRepository
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
 
     setContent {
-      AppTheme(dynamicColor = false) {
+      val isDarkMode = dataStoreRepo.getIsDarkModeEnabled().collectAsState(initial = false)
+
+      AppTheme(darkTheme = isDarkMode.value, dynamicColor = false) {
         QuickNotesNavHost()
       }
     }
